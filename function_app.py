8fe70fb87928840e0bc242b75d125ac73bb1e12d
Jtar0 <Jtaro_@outlook.com>
2026-09-08 01:35:17 +0000

    Add GitHub Actions deployment pipeline

--- function_app.py
+++ function_app.py
@@ -19,7 +19,7 @@ def howdy(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Howdy, {name}! Deployed automatically through GitHub Actions.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
